7041719639b56c596d224732655167bdb8078ac6
Subham044556 <subham04567@gmail.com>
2025-05-25 04:40:59 +0000

    learned datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,23 @@
+const accountId = 14453
+let accountEmail = "subham@123"
+var password = "12345"
+accountCity = " jaipur"
+let accountState;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hcemail.com"
+password = "jslfndfk0"
+accountCity ="BENGALURU"
+
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountCity,password,accountState])
