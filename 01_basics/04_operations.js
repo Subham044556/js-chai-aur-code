0f9a3fcb35b166307ca702386a9c1de3940e8828
Subham044556 <subham04567@gmail.com>
2025-05-25 06:30:39 +0000

    logic and conversions

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,18 @@
+let value = 5;
+let str1 = " Subham";
+let str2 = " Biswas";
+// let str3 = str1 + str2;
+console.log(str2 + str1);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2);
+console.log("1" + "2"  + 3);
+console.log(1 + 2 + "3");
+
+//Logic Operators --> ! - + =
+console.log("2" == 2 );// this converts the string to number and checks 
+// Avoid this type of conversions
+
+
+// === these are strict operators this only checks
+console.log("2" === 2);
