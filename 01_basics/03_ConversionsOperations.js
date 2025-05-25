0c7703091bc65f58516e3bdd88b4d4e25493004e
Subham044556 <subham04567@gmail.com>
2025-05-25 06:09:14 +0000

    Conversions of  datatypes

---
+++ 01_basics/03_ConversionsOperations.js
@@ -0,0 +1,22 @@
+let score = "skfkf";
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+ 
+// To number 
+// "33" => 33
+// "22abc"  => NaN
+// true => 1 , false => 0
+
+
+//To boolean
+// 1 => true, 0=> false
+//"anything" => true , "" => false
+
+
+// To strimg 
