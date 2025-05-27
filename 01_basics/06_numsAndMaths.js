893e4a7703a1c18745afd275de6b7bb0db409e5d
Subham044556 <subham04567@gmail.com>
2025-05-27 02:36:41 +0000

    maths and numbers

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,10 @@
+const score = 500
+console.log(score)
+
+//specified that balance is number only 
+const  balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)) // to display precision value 
+
