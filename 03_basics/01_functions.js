bf08e3dfa39ed503fd91f918ad19ad5099722c18
Subham044556 <subham04567@gmail.com>
2025-05-29 08:29:11 +0000

    scopes learned

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,4 +8,43 @@ function loginUserMessage(username = "sam"){ //this gave a default value
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+//
+
+
+function calculateCartPrice(...num1 ){// ... -> this is called rest operator as well as spread operator , but here it is rest because 
+    return num1;
+}
+
+function calculateCartPrice02(val1 , val2 ,...num1 ){// ... -> now this will take the rest as a array and return that part only  
+    return num1;
+}
+
+// console.log(calculateCartPrice02(200,400,600, 700,300));
+
+const user = {
+    username: "soham",
+    prices: 500,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.prices}`);
+}
+
+
+handleObject(user)
+
+handleObject({
+    username:"sam",
+    prices: 566,
+})
+
+const myNewArray = [200,300,400,1000];
+
+function return2ndValue(getArray){
+    return getArray[2]
+}
+
+console.log(return2ndValue(myNewArray))
+console.log(return2ndValue([200,500,600,400]))
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,60 @@
+
+
+let a = 10 
+const b =45
+var c = 67
+
+{
+
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// function one(){
+//     const username = "sunah"
+
+//     function two(){
+//         const website ="youtube";
+//         console.log(website);
+//     }
+//     // console.log(website);
+//     // two()
+//     console.log(username);
+// }
+
+// one()
+
+// if(true ){
+//     const username = "sona"
+//     if(username === "sona"){
+//         const website = "youtube"
+//         console.log(username+" "+website)
+//     }
+    //console.log(website);
+//}
+
+//console.log(username)
+
+
+function addOne(num){
+    return num + 1
+}
+
+//addOne(5)
+
+
+const addTwo = function (num){ // is type ke function me declaration k pehle call karne se error aata he , kyuki ye argument type treat kiyaa jaata he
+    console.log();
+    
+    return num + 2; 
+}
+
+addTwo(5)
+
+
+
+
