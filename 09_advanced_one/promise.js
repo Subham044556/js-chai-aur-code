5b460f8a8c5710f481889193a20a1507804f938b
Subham044556 <subham04567@gmail.com>
2025-06-17 09:43:51 +0000

    learning about promise

---
+++ 09_advanced_one/promise.js
@@ -0,0 +1,60 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //Do sdync tasks
+    //DB calls , cryptography, nretwork
+    setTimeout(function(){
+        console.log('Async task is completed');
+        resolve()
+    }, 1000);
+
+})
+
+promiseOne.then(function(){
+    console.log("Promise Consumed");
+    
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve();
+    },1000 )
+}).then(function(){
+    console.log('Async task 2 resolved');
+    
+})
+
+const promoseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username:"Chai" , email:"TeriMeriKahani"})
+    }, 1000)
+})
+
+promoseThree.then(function(user){
+    console.log(user);
+    
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username:"Subham", password:"1234"})
+        }else{
+            reject("ERROR: Something went wrong")
+        }
+    },1000)
+    
+})
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username;
+})
+.then((username)=>{
+    console.log(username);
+    
+})
+.catch(function(message){
+    console.log(message);
+})
